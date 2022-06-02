715f09709251b2edd335fc4b397174f556276af5
Pavel Prokopenya <prokopenya.work@gmail.com>
2022-06-02 16:31:38 +0300

    few objects created & filled

--- js_1/js/script.js
+++ js_1/js/script.js
@@ -1,2 +1,18 @@
 'use strict';
 
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let lastWatchedFilmName = prompt("Один из последних просмотренных фильмов?", "");
+let lastWatchedFilmMark = +prompt("На сколько вы оцените его?", "");
+
+personalMovieDB.movies[lastWatchedFilmName] = lastWatchedFilmMark;
+
+console.log(personalMovieDB);
